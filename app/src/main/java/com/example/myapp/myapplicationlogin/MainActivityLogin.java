1ec38b0101c9e29f44be594b77a2ce8bdbbdb502
WidiyaNurfitri16 <widiyanurfitri@student.uinsgd.ac.id>
2016-09-17 07:31:39 +0700

    Versi 2.1

---
+++ app/src/main/java/com/example/myapp/myapplicationlogin/MainActivityLogin.java
@@ -0,0 +1,13 @@
+package com.example.myapp.myapplicationlogin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity_login);
+    }
+}
